bcc4b532d8926587fc3f580ce48a5ca19b6e899b
nandiniramakrishnan <nramakri@andrew.cmu.edu>
2016-04-17 23:32:37 -0400

    latest changes 4/17

--- app.c
+++ app.c
@@ -23,18 +23,18 @@
 
 //define max buffer size
 #define BUF_SIZE 20
-
+#define DELAY 3
 /* --------- GLOBAL VARIABLES ---------- */
 
 // Counter used for each colour in the Timer Overflow Interrupt
-volatile unsigned long cyan_overflow_count = 0;
-volatile unsigned long magenta_overflow_count = 0;
-volatile unsigned long yellow_overflow_count = 0;
+volatile unsigned long cyan_overflow_count;
+volatile unsigned long magenta_overflow_count;
+volatile unsigned long yellow_overflow_count;
 
 // Variables to store received bytes for each colour from UART
-volatile uint8_t cyan = 0;
-volatile uint8_t magenta = 0;
-volatile uint8_t yellow = 0;
+volatile uint8_t cyan;
+volatile uint8_t magenta;
+volatile uint8_t yellow;
 
 // type definition of buffer structure
 typedef struct {
@@ -48,19 +48,19 @@ typedef struct {
 u8buf buf;
 
 // Global to turn motor on and off
-volatile uint8_t motor_on = 0;
+volatile uint8_t motor_on;
 
-unsigned long interval = 20;
-volatile unsigned long cyanVal = 0;
-volatile unsigned long magentaVal = 0;
-volatile unsigned long yellowVal = 0;
-volatile unsigned uint8_t finishedCount = 0;
+volatile unsigned long interval = 25;
+volatile unsigned long cyanVal;
+volatile unsigned long magentaVal;
+volatile unsigned long yellowVal;
+volatile uint8_t finishedCount;
 
 /* --------- METHODS CALLED DURING START UP --------- */
 
 /* pwm_init: Register settings for PWM to actuate the fuel injectors */
 void pwm_init() {
-  DDRD |= (1 << DDD6)                               // F1
+  DDRD |= (1 << DDD6);                              // F1
   DDRB |= (1 << DDB1) | (1 << DDB2);                // F2 and F3
 
   // F1 
@@ -84,16 +84,16 @@ void timer_init() {
   TCCR2B |= (1 << CS20) | (1 << CS22);      // Prescale by 1024
 }
 
-/* motor_init: Set motor pins as output */
+/* motor_init: Set motor pins as output and stop any rotation */
 void motor_init() {
   DDRC |= (1 << DDC0) | (1 << DDC1) | (1 << DDC2) | (1 << DDC3);
-  PORTC &= 0xF0;               // Initially, motor should be off 
+  PORTC &= 0xF0;              // 1111 0000
 }
 
 /* pump_init: Set pump pins as output */
 void pump_init() {
   DDRD |= (1 << DDD2) | (1 << DDD3) | (1 << DDD4);
-  PORTD &= 0xE3;              // Initially, pumps should be off
+  PORTD &= 0xE3;             // 1110 0011 
 }
 
 /* --------- END START UP METHODS --------- */
@@ -101,32 +101,31 @@ void pump_init() {
 
 /* timer_intt_on: Enable timer 2 overflow interrupt */
 void timer_intt_on() {
-  TIMSK2 |= 0x01;
+  TIMSK2 |= 0x01;           // 0000 0001
 }
 
 /* timer_intt_off: Disable timer 2 overflow interrupt */
 void timer_intt_off() {
-  TIMSK2 &= 0xFE;
-}
-
-/* pumps_on: Turn pumps on with 71% duty cycle */
-void pumps_on() {
-  OCR0A = 180;
-  OCR1A = 180;
-  OCR1B = 180;
+  TIMSK2 &= 0xFE;           // 1111 1110
 }
 
 /* Methods to turn each fuel injector on */
 void pwm1_on() {
-  OCR0A = 180;
+  OCR0A = 127;
 }
 
 void pwm2_on() {
-  OCR1A = 180;
+  OCR1A = 127;
 }
 
 void pwm3_on() {
-  OCR1B = 180;
+  OCR1B = 127;
+}
+
+void injectors_on() {
+  OCR0A = 127;
+  OCR1A = 127;
+  OCR1B = 127;
 }
 
 /* Methods to turn each fuel injector off */
@@ -168,16 +167,20 @@ void pump3_off() {            // PD4
   PORTD &= 0xEF;              // 1110 1111
 }
 
+void pumps_on() {
+  PORTD |= 0x1C;               // 0001 1100
+}
+
 /* rotate_motor: Moves the stepper motor */
 void rotate_motor() {
   PORTC = 0x01;          //0001
-  _delay_ms(10);
+  _delay_ms(DELAY);
   PORTC = 0x04;          //0100
-  _delay_ms(10);
+  _delay_ms(DELAY);
   PORTC = 0x02;          //0010
-  _delay_ms(10);
+  _delay_ms(DELAY);
   PORTC = 0x08;          //1000
-  _delay_ms(10);
+  _delay_ms(DELAY);
 }
 
 /* --------- UART Methods --------- */
@@ -186,6 +189,11 @@ void rotate_motor() {
 void buffer_init(u8buf *buf) {
   // set index to start of buffer
   buf->index = 0;
+  /*
+  int i = 0;
+  for (i = 0; i < BUF_SIZE; i++) {
+    buf->buffer[i] = 0;
+  }*/
 }
 
 int buf_empty(u8buf *buf) {
@@ -201,34 +209,42 @@ int buf_full(u8buf *buf) {
 }
 
 // write to buffer routine
-void buffer_write(u8buf *buf, uint8_t u8data) {
+void buffer_write(u8buf *buf, volatile uint8_t u8data) {
   if (!buf_full(buf)) {
     buf->buffer[buf->index] = u8data;
     // increment buffer index
     buf->index++;
   }
   if (buf->index == 1) {
+    // store the received colour value
     cyan = u8data;
+    // scale up the amount of time you need the pump to be on
+    cyanVal = cyan*interval;
     pump1_on();
     pwm1_on();
-    cyanVal = cyan*interval;
   }
   else if (buf->index == 2) {
+    magentaVal = magenta*interval;
     magenta = u8data;
     pump2_on();
     pwm2_on();
-    magentaVal = magenta*interval;
   }
   else if (buf->index == 3) {
     yellow = u8data;
+    yellowVal = yellow*interval;
+    // received all values. Turn on pumps and injectors
     pump3_on();
     pwm3_on();
-    yellowVal = yellow*interval;
+    // Disable the receiver and the receive complete interrupt
+    // UCSR0B &= 0x6F;             // 0110 1111
   }
-  motor_on = 1;
+  // Start spinning the motor in the main function
+  motor_on = 1;                
+  // Enable timer interrupts to check completion of the three colours
   timer_intt_on();
 }
 
+/* Not currently being used. Needed to transmit things to app */
 void buffer_read(u8buf *buf, volatile uint8_t *u8data) {
   if (!buf_empty(buf)) {
     buf->index--;
@@ -256,8 +272,18 @@ void reset_printer() {
   motor_init();
   pump_init();
   uart_init();
+  motor_on = 0;
   timer_intt_off();
   finishedCount = 0;
+  cyan = 0;
+  magenta = 0;
+  yellow = 0;
+  cyanVal = 0;
+  magentaVal = 0;
+  yellowVal = 0;
+  cyan_overflow_count = 0;
+  magenta_overflow_count = 0;
+  yellow_overflow_count = 0;
 }
 
 /* ---------- Interrupt Subroutines --------- */
@@ -294,7 +320,7 @@ ISR(TIMER2_OVF_vect) {
   if (cyan_overflow_count == cyanVal) {
     pump1_off();
     pwm1_off();
-    finshedCount++;
+    finishedCount++;
   }
   if (magenta_overflow_count == magentaVal) {
     pump2_off();
@@ -306,8 +332,8 @@ ISR(TIMER2_OVF_vect) {
     pwm3_off();
     finishedCount++;
   }
+  // all colours are done spraying
   if (finishedCount == 3) {
-    timer_intt_off();
     reset_printer(); 
   }
   cyan_overflow_count++;
@@ -317,14 +343,10 @@ ISR(TIMER2_OVF_vect) {
 
 /* ----- End interrupt subroutines ----- */
 
-/* ----- Main routine ----- */
+/* ----- Main routine. Only operating the motor here ----- */
 int main() {
   /* Startup methods */
-  pwm_init();
-  timer_init();
-  motor_init();
-  pump_init();
-  uart_init();
+  reset_printer();
   sei();			              /* Enable interrupts */
   while (1) {
     if (motor_on) {
